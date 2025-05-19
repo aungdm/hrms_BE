3038bff30c1701a3ba450dcd431ff5ececd8d540
aungdm <aungdm520@gmail.com>
2025-05-20 00:53:49 +0500

    testing setup-4

--- src/app.js
+++ src/app.js
@@ -81,13 +81,13 @@ connectDB();
 
 // Define all routes
 app.use("/api/v1/employee", require("./routes/employeeRoutes"));
-app.use("/api/v1/salaryRevisions", require("./routes/salaryRevisionsRoutes"));
-app.use("/api/v1/attendanceLogs", require("./routes/attendanceLogsRoutes"));
-app.use("/api/v1/timeSheet", require("./routes/timeSheetRoutes"));
-app.use("/api/v1/dailyAttendance", require("./routes/dailyAttendanceRoutes"));
-app.use("/api/v1/workSchedule", require("./routes/workScheduleRoutes"));
-app.use("/api/v1/leave", require("./routes/leaveRoutes"));
-app.use("/api/v1/punch", require("./routes/punchRoutes"));
+// app.use("/api/v1/salaryRevisions", require("./routes/salaryRevisionsRoutes"));
+// app.use("/api/v1/attendanceLogs", require("./routes/attendanceLogsRoutes"));
+// app.use("/api/v1/timeSheet", require("./routes/timeSheetRoutes"));
+// app.use("/api/v1/dailyAttendance", require("./routes/dailyAttendanceRoutes"));
+// app.use("/api/v1/workSchedule", require("./routes/workScheduleRoutes"));
+// app.use("/api/v1/leave", require("./routes/leaveRoutes"));
+// app.use("/api/v1/punch", require("./routes/punchRoutes"));
 
 // Add a health check route for Vercel
 // app.get("/api/health", (req, res) => {
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
+    console.log(`MongoDB connected successfully ${process.env.MONGO_URI}`);
   } catch (error) {
     console.error(error);
     process.exit(1);
