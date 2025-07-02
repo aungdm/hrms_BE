062ab7ca646e07ae5bc2cc6a3e5ce58f96d90915
aungdm <aungdm520@gmail.com>
2025-07-02 08:57:42 +0500

    fixes

--- src/routes/otherDeductionsRoutes.js
+++ src/routes/otherDeductionsRoutes.js
@@ -7,7 +7,8 @@ const {
   getDeduction,
   deleteDeduction,
   updateDeductionStatus,
-  deleteMultipleDeductions
+  deleteMultipleDeductions,
+  updateProcessedStatus
 } = require("../controllers/otherDeductionsController");
 
 // Routes for other deductions
@@ -17,6 +18,7 @@ router.get("/", getDeductions);
 router.get("/:id", getDeduction);
 router.delete("/:id", deleteDeduction);
 router.patch("/:id/status", updateDeductionStatus);
+router.patch("/:id/processed", updateProcessedStatus);
 router.post("/delete-multiple", deleteMultipleDeductions);
 
 module.exports = router; 
